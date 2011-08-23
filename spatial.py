ab200dbad98808c1f20c041cbe05924113f764e1
lesh <lesh@sysphere.org>
2011-08-23 04:57:50 +0200

    bugfixes

--- spatial.py
+++ spatial.py
@@ -9,36 +9,36 @@ class geo_notify():
         self.usercollection.remove()
     def locationpublish(self,user,loc,sessionid):
         # to notify user via the connection of new users nearby
+        user = {"username": user.username, "sharelocation": user.sharelocation, "time": time.time(), "sessionid": sessionid, "loc": loc}
 
-
-        self.usercollection.remove({"username": user.username,"sessionid":sessionid})
-        self.usercollection.insert({"username": user.username, "sharelocation": user.sharelocation, "time": time.time(), "sessionid": sessionid, "loc": loc})
-
-
-        n = self.userneighbours(user,loc)
+        self.usercollection.remove({"username": user['username'],"sessionid":user['sessionid']})
+        self.usercollection.insert(user)
+        
+        n = self.userneighbours(user)
 
         # inform user about neighbours
         self.message(sessionid,{ "command": "geo_list", "data": n.keys()})
 
         # inform neighbours about user
         for nuser in n.itervalues():
-            map(lambda sessionid: self.message(sessionid,{ "command": "geo_add", "data": user.username }),nuser['sessionid'])
+            map(lambda sessionid: self.message(sessionid,{ "command": "geo_add", "data": user['username'] }),nuser['sessionid'])
 
     def message(self,sessionid,data):
         self.queue.master.get('#' + sessionid).send(data)
             
     def logout(self,sessionid):
-        
-        user = self.usercollection.find_one({"sessionid":sessionid})        
-        n = self.userneighbours(user,user['loc'])
-        for nuser in n:
-            map(lambda sessionid: self.message(sessionid,{ "command": "geo_del", "data": user.username }),n['sessionid'])
+        user = self.usercollection.find_one({"sessionid":sessionid})
+        print ("found user",user)
+        n = self.userneighbours(user)
+        print ("found n",n)
+        for nuser in n.itervalues():
+            map(lambda sessionid: self.message(sessionid,{ "command": "geo_del", "data": user['username'] }),nuser['sessionid'])
 
-    def userneighbours(self,user,loc):
+    def userneighbours(self,user):
         n = {}
-        for nuser in self.neighbours(loc):
+        for nuser in self.neighbours(user['loc']):
             print ("WORKING ON",nuser)
-            if (nuser['username'] != user.username):
+            if (nuser['username'] != user['username']):
                 if (n.has_key(nuser['username'])):
                     n[nuser['username']]['sessionid'].append(nuser['sessionid'])
                 else:
